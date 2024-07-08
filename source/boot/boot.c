a6aff960bb8412f71c3fcfe11e1ee26b4e4b7e4c
Riley Hawksworth <rileyrooh13@gmail.com>
2024-07-08 19:15:53 +0100

    Fix 'IntToStr' function.

--- source/boot/boot.c
+++ source/boot/boot.c
@@ -98,22 +98,27 @@ void UintToStr(UINTN integer, CHAR16* string) {
     }
 }
 
-/*
 void IntToStr(INTN integer, CHAR16* string) {
     UINTN index = 0;
+    UINT8 negative = 0;
+
+    if (integer < 0) {
+        negative = 1;
+        integer *= -1;
+    }
 
     do {
         string[index++] = integer % 10 + L'0';
     } while ((integer /= 10) > 0);
-    string[index] = L'\0';                                          
+
+    if (negative > 0) {
+        string[index++] = '-';
+    }
+
+    string[index] = L'\0';
 
     UINTN start = 0;
     UINTN end = index - 1;
-
-    if (integer < 0) {
-        string[index] = '-';
-        index++;
-    }
 
     while (start < end) {
         CHAR16 temp = string[start];
@@ -124,4 +129,3 @@ void IntToStr(INTN integer, CHAR16* string) {
         end--;
     }
 }
-*/
